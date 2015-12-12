b5d42cdd3e951c5fc2d687312acebbdd55d729f8
Todmy <tolok.d@gmail.com>
2015-12-12 18:56:45 +0200

    Added gulp with some errros

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,5 +1,7 @@
 module.exports = function(gulp, plugins) {
+  console.log(plugins)
   return gulp.task('clean', function() {
+    console.log(plugins.clean)
     return gulp.src('dist', { read: false })
       .pipe(plugins.clean());
   });
