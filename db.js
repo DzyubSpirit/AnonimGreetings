8bb0bd111b9041de88b0414e678206b0dfa80dd4
Vlad <Dzyubavlad@gmail.com>
2015-12-13 00:13:01 +0200

    Aded db queries for quests

--- db.js
+++ db.js
@@ -65,3 +65,36 @@ exports.validPassword = function(user_obj, callback) {
         return callback(null, true);
     });
 }
+
+exports.getQuests = function(user_id, callback) {
+    var sql =
+"SELECT user_quests.user_id, \
+        user_quests.quest_id, \
+        quests.text \
+FROM `users` \
+RIGHT JOIN `user_quests` \
+ON users.id=? AND users.id=user_quests.user_id \
+LEFT JOIN `quests` \
+ON user_quests.quest_id=quests.id;"
+    var inserts = [user_id];
+    sql = mysql.format(sql, inserts, true);
+    connection.query(sql, function(err, data) {
+        if (err) {
+            return callback(err);
+        }
+        if (data.length === 0) {
+            return callback(null, false);
+        }
+        var res = {
+            user_id: data[0]['user_id'],
+            quests: {}
+        };
+        console.log(data);
+        for (var i in data) {
+            console.log(data[i]['quest_id']);
+            console.log(data[i]['quest_text']);
+            res.quests[data[i]['quest_id']] = data[i]['text'];
+        }
+        callback(null, res);
+    });
+}
