b2d970305f6e5b2964340b8456c6ed3d2b1390cb
Todmy <tolok.d@gmail.com>
2015-12-12 23:16:22 +0200

    Atom bugs:(

--- tasks/compile-js.js
+++ tasks/compile-js.js
@@ -1,19 +1,10 @@
-var es = require('event-stream');
-var browserify = require('gulp-browserify');
-var globby = require('globby');
-
 module.exports = function(gulp, plugins) {
   return gulp.task('compile-js', function() {
-      var js = browserify({
-        debug: true,
-        entries: globby.sync([
-          'src/lib/**/*.js',
-          'src/app/*.js'
-        ])
-      })
-
-    return es.merge(js)
+    return gulp.src([
+        'src/app/*.js'
+      ])
       .pipe(plugins.concat('app.js'))
+      .pipe(plugins.md5(10))
       .pipe(gulp.dest('public'));
   });
 };
--- tasks/concat-vendors.js
+++ tasks/concat-vendors.js
@@ -2,8 +2,7 @@ module.exports = function(gulp, plugins) {
   return gulp.task('concat-vendors', function() {
     return gulp.src([
         'node_modules/angular/angular.js',
-        'node_modules/ui-router/release/angular-ui-router.js',
-        'node_modules/ui-select/dist/select.min.js'
+        'node_modules/ui-router/release/angular-ui-router.js'
       ])
       .pipe(plugins.concat('vendors.js'))
       .pipe(plugins.md5(10))
