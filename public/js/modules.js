c59376dc2b378d7f5d67eaebb216da1bf66cb0ec
Todmy <tolok.d@gmail.com>
2015-12-13 11:53:14 +0200

    main page begin

--- public/js/modules.js
+++ public/js/modules.js
@@ -1,4 +1,5 @@
-angular.module('app', [ 'ui.router', 'app.configuration', 'ngMockE2E', 'app.auth', 'app.rest' ]);
+angular.module('app', [ 'ui.router', 'app.configuration', 'ngMockE2E', 'app.auth', 'app.rest', 'app.main' ]);
 angular.module( 'app.configuration', []);
 angular.module( 'app.auth', []);
+angular.module( 'app.main', []);
 angular.module( 'app.rest', [ 'ngResource' ]);
